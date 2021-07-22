69f648611361f82231f4602c0d09c111fc4f36f6
xiaoas <xiaoqilin82@gmail.com>
2021-07-22 22:09:44 +0800

    init skill export demo

--- extension.js
+++ extension.js
@@ -26,6 +26,47 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
                     ans += JSON.stringify(obj)
                     navigator.clipboard.writeText(ans).then(()=> console.log('success'))
                 },
+                skills(refNode) {
+                    var targetCharacters = Object.keys(lib.character).filter(c => c.startsWith('jlsg'));
+                    // TODO: sort by in-game order
+                    var targets = new Set()
+                    for (c of targetCharacters) {
+                        var skills = lib.character[c][3]
+                        var obj = {};
+                        var addSkill = function (skill) {
+                            var info = get.translation(skill + '_info')
+                            if (skill in obj || !info) {
+                                return;
+                            }
+                            // TODO: make skill prefix, card name, skill name changes
+                            var repalceInfo = {
+                                '锁定技，': '锁定技',
+                                '主公技，': '主公技',
+                                '限定技，': '限定技',
+                                '觉醒技，': '觉醒技',
+                            }
+                            var skillDef = {
+                                name: get.translation(skill),
+                                intro: get.translation(skill + '_info'),
+                            }
+                        }
+                        for (var skill of skills) {
+                            addSkill(skill);
+                            if (lib.skill[skill].derivation) {
+                                [lib.skill[skill].derivation].flat().forEach(s => addSkill(s));
+                            }
+                        }
+                    }
+                    var ans = `export const skill = <SkillCollection>`;
+                    ans += JSON.stringify(obj)
+                    navigator.clipboard.writeText(ans).then(()=> console.log('success'))
+                },
+                cards(refNode) {
+
+                },
+                pile(refNode) {
+
+                },
             }
         },
         precontent: function () {
@@ -50,11 +91,11 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
                 translate: {
                 },
             },
-            intro: `This extension aims to export jilue to a v2-friendly format. note that this might be subject to breaking changes given the early state of noname-v2.<br>
-<span onclick="if(jlsg_export)jlsg_export.characters(this)">export characters</span><br>
-<span>export skills</span><br>
-<span>export cards</span><br>
-<span>export pile</span><br>
+            intro: `This extension aims to export jilue to a v2-friendly format. note that this might be subject to breaking changes given the early state of noname-v2.<br><br>
+<span style="cursor: pointer;text-decoration: underline;font-weight: bold;" onclick="jlsg_export.characters(this)">export characters</span><br>
+<span style="cursor: pointer;text-decoration: underline;font-weight: bold;" onclick="jlsg_export.skills(this)">export skills</span><br>
+<span style="cursor: pointer;text-decoration: underline;font-weight: bold;" onclick="jlsg_export.cards(this)">export cards</span><br>
+<span style="cursor: pointer;text-decoration: underline;font-weight: bold;" onclick="jlsg_export.pile(this)">export pile</span><br>
 `,
             author: "xiaoas",
             diskURL: "",
